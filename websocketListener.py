ba051925f2a1337e555ab548ac911f0525f5d7db
Ubuntu <ubuntu@ip-172-31-14-47.us-east-2.compute.internal>
2023-08-25 00:35:05 +0000

    socket server is getting data from server via pipe and sending to client

--- websocketListener.py
+++ websocketListener.py
@@ -6,48 +6,83 @@ import signal
 import sys
 import websockets
 import time
+import threading
+from asyncio.events import AbstractEventLoop
 
-# IPC message queue
+# Dictionary to store connected clients
+connected_clients = {}
 
 
-async def send_message(websocket):
-    global ipc_queue
+def pipe_input():
+    print("enter send")
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     while True:
-        
-        print("ipc get")
-        with open('pipe1') as f:   # add `rb` for binary mode
-            # line-by-line read
-            for line in f:
-                print(line)
-                await websocket.send(line)
-                print(f"Sent: {line}")
+        for websocket in connected_clients:
+            print("socket")
+            with open('pipe1') as f:   # add `rb` for binary mode
+                print("open pipe")
+                # line-by-line read
+                for line in f:
+                    print ("read line")
+                    print(line)
+                    loop.run_until_complete(send_message(websocket, line))
+                    print(f"Sent: {line}")
+        print("sleep")
         time.sleep(1)
+        
 
-async def receive_message(websocket):
-    message = await websocket.recv()
-    print(f"Received: {message}")
+async def send_message(websocket, message):
+        print ('wait tx')
+        await websocket.send(message)
+        print(f"sent: {message}")
 
 
-# WebSocket server
-async def websocket_server(websocket, path):
-    print("go")
-    await asyncio.gather(
-        send_message(websocket),
-        receive_message(websocket)
-    )
+async def client_handler(websocket, path):
+    # Add client to dictionary
+    connected_clients[websocket] = path
+    print(f"Client connected: {path}")
+    try:
+        async for message in websocket:
+            print(f"Received from {path}: {message}")
+            
+            # Echo the message back to the client
+            await websocket.send(f"You said: {message}")
 
+    except websockets.exceptions.ConnectionClosed:
+        pass
+
+    # Remove client from dictionary when disconnected
+    del connected_clients[websocket]
+    print(f"Client disconnected: {path}")
+
+
+
+async def main(func1):
+    await asyncio.gather(func1)
 
 if __name__ == "__main__":
 
+   
 
     # Start WebSocket server
-    start_server = websockets.serve(websocket_server, "0.0.0.0", 12345)
+    start_server = websockets.serve(client_handler, "0.0.0.0", 12345)
 
     try:
+        t1 = threading.Thread(target=pipe_input)
+        t1.start()
+        #asyncio.get_event_loop().run_until_complete(main(send_message()))
+        #asyncio.get_event_loop().create_task(send_message())
+
         asyncio.get_event_loop().run_until_complete(start_server)
+        print("start server")
         asyncio.get_event_loop().run_forever()
+
     except KeyboardInterrupt:
-        ipc_process.terminate()
-        ipc_process.join()
         print("WebSocket server terminated.")
         sys.exit(0)
+
+
+
+
+
